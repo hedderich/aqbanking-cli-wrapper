5b0eb0152748ef0f044df592a0fcc6cdebbbae1c
Marvin Hedderich <hedderich@users.noreply.github.com>
2017-01-15 15:36:11 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='aqbanking-cli-wrapper',
+    version='0.1.0',
+    packages=['aqbanking'],
+    license='Apache-2.0',
+    url='https://github.com/hedderich/aqbanking-cli-wrapper',
+    author='Marvin Hedderich',
+    author_email='git@hedderich.info',
+    description='Fetch bank account data through AqBanking'
+)
