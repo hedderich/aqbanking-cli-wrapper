0b3c90e59c0f12fc94b7f16557b3320bf8b7c9a1
Marvin Hedderich <hedderich@users.noreply.github.com>
2017-06-15 14:34:11 +0200

    Update contact details

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(
     license='Apache-2.0',
     url='https://github.com/hedderich/aqbanking-cli-wrapper',
     author='Marvin Hedderich',
-    author_email='git@hedderich.info',
+    author_email='dev@hedderich.info',
     description='Fetch bank account data through AqBanking'
 )
